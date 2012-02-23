1d01454ef05b7bab457f6cf730204196afa3c5dd
ryan508 <ryan.p.curtin@gmail.com>
2012-02-22 23:40:24 -0600

    Search results working with error handling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,13 +7,15 @@ class SearchController < ApplicationController
        redirect_to root_url
     else
        city = params[:city]
+         city.gsub!(" ", "+") if city.include?(" ")
        country = params[:country]
+         country.gsub!(" ", "+") if country.include?(" ")
        date = params[:date]
        nights = params[:nights]
     end
     
     @url = "https://affiliate.xsapi.webresint.com/1.1/propertylocationsearch.json?consumer_key=rankandmile.com&consumer_signature=5a50436e660bee116ec0bbcff7ef018aa1637cab&Country=#{country}&City=#{city}&DateStart=#{date}&NumNights=#{nights}"
-    @searchresults = HTTParty.get(@url)
+    @searchresults = HTTParty.get(@url).parsed_response
     
     
     
