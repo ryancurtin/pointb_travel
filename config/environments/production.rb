3530e97c6eb72364ad03ffbce9826b4ddd4a13ed
ryan508 <ryan.p.curtin@gmail.com>
2012-02-26 23:24:08 -0600

    Trying to get rid of heroku errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Hostelapp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  ::ActiveSupport::Deprecation.silenced = true
+
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
